a96e06c06e7d25b00a37a4da24369a8c17c4c997
txaviers <tiagoxsilva2@gmail.com>
2022-09-25 05:38:51 -0300

    Método recursivo

--- Fibonacci/fibonacci.c
+++ Fibonacci/fibonacci.c
@@ -2,26 +2,16 @@
 
 //Função para encontrar o n-ésimo termo da Seq. de Fibonacci começando por 0
 unsigned int fibonacci(unsigned int n){
-    
-    int i, fib, a = 0, b = 1;
-    
-    for(i=0; i<n; i++){
-        //O primeiro termo é considerado como 0
-        if(i==0){
-            fib = 0;
-        }
-        //O segundo termo é então 1
-        else if(i==1){
-            fib = 1;
-        }   
-        //Segue-se os demais termos da sequência
-        else{
-            fib = a + b;
-            a = b;
-            b = fib;
-        }
+    if(n==1){
+        //Primeiro termo 
+        return 0;        
     }
-    //Retorna-se o n-ésimo termo
-    return fib;
-
+    else if(n==2){
+        //Segundo termo 
+        return 1;        
+    }
+    else{
+        //Utiliza-se a expressão de recursão da sequência
+        return fibonacci(n-1) + fibonacci(n-2); 
+    }
 }
