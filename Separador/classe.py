1207a55fb13739330199b069da54c77bee67b299
txaviers <tiagoxsilva2@gmail.com>
2022-10-13 16:10:38 -0300

    Versão inicial funcionando

--- Separador/classe.py
+++ Separador/classe.py
@@ -2,18 +2,30 @@ class Frases():
     def __init__(self, filename:str):
 
         self.file = open(filename,'r')
-        self.dados = self.file.readlines()
-        for i in self.dados:
-            print(i)
-
+        self.dados = self.file.read()  
+   
     def mudar_separador(self,separador_atual:str,novo_separador:str):
-        pass
-        
+
+        self.lt_dados = list(self.dados)       
+        flag_aspas = False
+
+        for i in range(len(self.lt_dados)):
+
+            if not flag_aspas:
+                if self.lt_dados[i] == '"':
+                    flag_aspas = True        
+                elif self.lt_dados[i] == separador_atual:                    
+                    self.lt_dados[i] = novo_separador
+
+            elif self.lt_dados[i] == '"' and flag_aspas:
+                flag_aspas = False   
+
+        print(''.join(self.lt_dados))               
         
 
 if __name__ == '__main__':
 
-    frases_curtas = Frases('dados.txt')
- 
+    frases_curtas = Frases('arquivo_dados.txt')
+    frases_curtas.mudar_separador(",",";")
 
 
