f68d20c2bf94f830c10a94f151334af075e3ee38
txaviers <tiagoxsilva2@gmail.com>
2022-10-18 17:23:59 -0300

    Método com nova lógica em regex

--- Separador/classe.py
+++ Separador/classe.py
@@ -1,3 +1,5 @@
+import re
+
 #Implementa-se a classe proposta para manipular arquivo
 class ArquivoTXT():
     def __init__(self, filename:str):
@@ -12,26 +14,9 @@ class ArquivoTXT():
 
     #Método para mudar o separador
     def mudar_separador(self, separador_atual:str, novo_separador:str):
-        #Transforma-se a string em uma lista de caracteres 
-        self.lt_dados = list(self.dados)     
-        #Flag para não mudar vírgulas de dados entre aspas  
-        flag_aspas = False
-
-        #Percorre-se cada caracter do conteúdo
-        for i in range(len(self.lt_dados)):            
-            if not flag_aspas:
-                #Caso seja identificado uma primeira aspa, levanta-se a flag
-                if self.lt_dados[i] == '"':
-                    flag_aspas = True        
-                #Enquanto a flag estiver desligada, os separadores são mudados
-                elif self.lt_dados[i] == separador_atual:                    
-                    self.lt_dados[i] = novo_separador
-            #Identificada a última aspa de um dos dados, abaixa-se a flag
-            elif self.lt_dados[i] == '"' and flag_aspas:
-                flag_aspas = False   
-        
-        #Transforma-se a lista de caracteres novamente em string
-        self.dados = ''.join(self.lt_dados)
+        #Expressão regular para identificar separador fora de dados com aspas
+        regex_expression = separador_atual + '(?=(?:[^"]|"[^"]*")*$)'    
+        self.dados = re.sub(regex_expression, novo_separador, self.dados)
 
     #Método para salvar o contéudo no mesmo ou em um novo arquivo
     def salvar_arquivo(self, filename:str):        
